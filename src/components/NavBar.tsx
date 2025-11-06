990b9612c8e795a48530d40f58a32bff4ae6e633
sakilalakmal <sakilalakmal77@gmail.com>
2025-11-06 17:40:52 +0530

    initial Commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+import DesktopNavbar from "./DesktopNavBar";
+import MobileNavbar from "./ModileNavBar";
+import { currentUser } from "@clerk/nextjs/server";
+import { SyncUserDB } from "@/actions/user.actions";
+
+async function NavBar() {
+
+  const user = await currentUser();
+
+  if(user) await SyncUserDB();
+
+
+
+
+  return (
+    <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
+      <div className="max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              Vibely
+            </Link>
+          </div>
+
+          {/* desktop and mobile nav bar */}
+          <DesktopNavbar />
+          <MobileNavbar />
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
